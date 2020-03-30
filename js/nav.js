e1c8001d6a074ca7219d9d5b26b4507a8a30681d
Joel <winter-joel@outlook.de>
2020-03-30 14:58:25 -0400

    first commit

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+console.clear();
+
+const app = (() => {
+	let body;
+	let menu;
+	let menuItems;
+	
+	const init = () => {
+		body = document.querySelector('body');
+		menu = document.querySelector('.menu-icon');
+		menuItems = document.querySelectorAll('.nav__list-item');
+
+		applyListeners();
+	}
+	
+	const applyListeners = () => {
+		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+		menuItems.forEach(element => {
+			element.addEventListener('click', () => toggleClass(body, 'nav-active'));
+		});
+	}
+	
+	const toggleClass = (element, stringClass) => {
+		if(element.classList.contains(stringClass))
+			element.classList.remove(stringClass);
+		else
+			element.classList.add(stringClass);
+	}
+	
+	init();
+})();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
